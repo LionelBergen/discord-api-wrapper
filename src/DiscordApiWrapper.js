6e3a933bd220b23edb74d5f4f85d5db2d5b706d2
dustytrash <Lionelbergen@live.com>
2022-01-02 15:46:14 -0600

    added send message function

--- src/DiscordApiWrapper.js
+++ src/DiscordApiWrapper.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 
+const DefaultCommandPrefix = '!';
+
 class DiscordBot {
-  constructor({discordToken, commandPrefix = '!'}) {
+  constructor({discordToken, commandPrefix = DefaultCommandPrefix}) {
     this.discordToken = discordToken;
     this.discordClient = null;
     this.commandPrefix = commandPrefix;
+    this.commands = [];
   }
 
   initialize() {
     return new Promise((resolve, reject) => {
       const client = new Discord.Client();
       this.discordClient = client;
+      const self = this;
 
       client.on('ready', () => {
         console.log(`Client ready, user.tag is: ${client.user.tag}`);
@@ -18,6 +22,27 @@ class DiscordBot {
       
       client.login(this.discordToken).then(function() {
         console.log(`Logged in as ${client.user.tag}`);
+
+        client.on('message', function(interaction) {
+          if (self.commands.length > 0) {
+            const messageContent = interaction.content.trim();
+            if (messageContent.startsWith(self.commandPrefix)) {
+              let command = messageContent.substring(self.commandPrefix.length);
+              command = command.replace(/(\r\n|\n|\r)/gm, ' ');
+
+              if (command.includes(' ')) {
+                command = command.substring(0, command.indexOf(' '));
+              }
+
+              const commandFound = self.commands.find(e => e.command == command);
+
+              if (commandFound) {
+                commandFound.commandFunction({discordMessageObject: interaction});
+              }
+            }
+          }
+        });
+
         resolve(client);
       }).catch(reject);
 
@@ -27,6 +52,14 @@ class DiscordBot {
         throw error;
       });
     });
+  }
+
+  sendMessage(channelName, message) {
+    this.discordClient.channels.get(channelName).send(message);
+  }
+
+  addOnCommandFunction({commandName, commandFunction}) {
+    this.commands.push({command: commandName, commandFunction: commandFunction});
   }
 }
 
